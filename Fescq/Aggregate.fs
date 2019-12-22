1b4a494e1c768498fbe6347db591e114dd6b97c0
jltrem <joe@lariat.io>
2019-12-21 19:57:53 -0600

    WIP F# Event Store, Commands, Queries

---
+++ Fescq/Aggregate.fs
@@ -0,0 +1,38 @@
+﻿namespace Fescq.Core
+
+module Aggregate = 
+
+   let noEvents = List<Event>.Empty
+   let oneEvent e = List<Event>.Cons(e, [])
+     
+
+[<AbstractClass>]
+type Aggregate (history:Event list, future:Event list, apply:Aggregate -> int -> Event -> unit) as self = 
+
+   let events = 
+      history @ future
+      |> function
+         | [] -> failwith "events cannot be empty"
+         | all -> 
+            all
+            |> List.map(fun x -> x.AggregateKey.Id)            
+            |> List.distinct
+            |> function
+               | [_] -> all
+               | _ -> failwith "events must refer to the same aggregate id"
+
+   let key = 
+      events 
+      |> List.last
+      |> fun x -> x.AggregateKey
+
+   // Apply all events with the provided function.
+   // This function must validate domain rules so that the 
+   // construction is successful IFF the state is valid
+   do 
+      events 
+      |> List.iteri (fun i e -> apply self (i + 1) e)   
+
+   member x.Key : AggregateKey = key
+   member x.NewEvents : Event list = future
+   
---
+++ Fescq/Core.fs
@@ -0,0 +1,26 @@
+﻿namespace Fescq.Core
+
+open System;
+
+/// This marks an event data class
+type IEventData = interface end
+
+/// All IEventData classes must be marked with this attribute which specifies revision
+[<AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)>]
+type EventDataAttribute(name:string, version:int) = 
+   inherit Attribute()
+   member x.Name with get() = name
+   member x.Version with get() = version
+
+type AggregateKey = {
+   Name: string
+   Id: Guid
+   Version: int
+}
+
+type Event = {
+   AggregateKey: AggregateKey
+   Timestamp: DateTimeOffset
+   MetaData: string
+   EventData: IEventData
+}
